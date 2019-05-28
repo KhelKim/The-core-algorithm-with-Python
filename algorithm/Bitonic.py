0e122c0bea9dbc7d4665e3ea8ed7d0ff58b8f4de
Khel-Kim <cafehero123@gmail.com>
2019-05-28 15:10:56 +0900

    NumberValley

--- algorithm/Bitonic.py
+++ algorithm/Bitonic.py
@@ -1,2 +1,19 @@
 input_numbers = [1, 5, 2, 1, 4, 3, 4, 5, 2, 1]
 
+
+def find_candi(arr):
+    up = []
+    down = []
+    if len(arr) == 1:
+        return False
+    else:
+        first = arr[0]
+        for i in arr[1:]:
+            if i > first:
+                up.append(i)
+            elif i < first:
+                down.append(i)
+        return up, down
+
+
+print(find_candi(input_numbers))
